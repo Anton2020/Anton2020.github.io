1564c9612617c2a97b5ac1844c48f16f8e8d4eb4
Ewout Lagendijk <koningcoding@gmail.com>
2019-02-25 19:43:11 +0100

    Tickybot

---
+++ ticky.js
@@ -0,0 +1,19 @@
+function start_pos()
+{
+document.getElementById("eyes").style.top = '550px';
+document.getElementById("eyes").style.left = '550px'
+}
+start_pos()
+function right() //Moves image to the right
+
+{
+document.getElementById("eyes").style.top = '400px';
+document.getElementById("eyes").style.left = '1000px';
+}
+
+function left() //moves image to the left
+
+{
+document.getElementById("eyes").style.top = '0px';
+document.getElementById("eyes").style.left = '0px';
+}
