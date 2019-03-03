3d83fd47f0a2d685d57f37e3c299028f923b35a9
Ewout Lagendijk <koningcoding@gmail.com>
2019-03-03 12:59:11 +0100

    Add files via upload

---
+++ searchforStringwithinSubstring.js
@@ -0,0 +1,24 @@
+// JavaScript source code
+function searchForSubstring(stringToSearch, stringToFind) {
+
+    if (stringToSearch === '' || stringToFind === '') {
+        return (-1);
+    }
+
+    else if (stringToFind === null && stringToSearch === null) {
+        return (-1);
+    }
+
+    else if (stringToFind.length > stringToSearch.length) {
+        return (-1);
+    }
+
+    else if (stringToSearch.includes(stringToFind)) {
+        return (stringToSearch.indexOf(stringToFind));
+    }
+
+    else {
+        return (-1);
+    }
+
+}
